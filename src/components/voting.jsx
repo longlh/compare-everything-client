030f8b01db34dcbf4bd63d8b1f9b2ba6368aa9c0
LongLH <d@dapps.me>
2017-04-18 14:44:32 +0700

    update code with TDD style

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -11,7 +11,8 @@ class Voting extends React.Component {
 		return (
 			<div className='voting'>
 				{this.getPair().map(entry =>
-					<button key={entry}>
+					<button key={entry}
+						onClick={() => this.props.vote(entry)}>
 						<h1>{entry}</h1>
 					</button>
 				)}
